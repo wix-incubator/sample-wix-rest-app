f7166b2fb5cd976dba9be5607a23f6c904329148
Shay Cohen <shayco@shay-co-mbp13.local>
2019-02-26 10:58:57 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Quick start
+
+0. clone this repo
+    git clone XXX
+1. update APP_ID with your application id
+2. update APP_SECRET with your application seceret
+3. update WEBHOOK_VERIFICATION_ID with your WEBHOOK VERIFICATION ID
+4. run ngrok to get https proxy to your server:
+    ngrok http 3000
+5. configure "Redirect URL" in Wix Developpers to https://[Your ngrok identity].ngrok.io/login
+6. configure "App URL" in Wix Developpers to https://[Your ngrok identity].ngrok.io/signup
+7. Run your server
+    npm install
+    npm build
+    npm start
---
+++ src/index.js
@@ -0,0 +1,141 @@
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+
+// TODO: Update APP_ID with your application id (can be found in in Wix Developpers under Workspace/OAuth)
+const APP_ID = 'a9e09516-230f-4855-9449-698f07061193';
+
+// TODO: Update APP_SECRET with your application secret (can be found in Wix Developpers under Workspace/OAuth)
+const APP_SECRET = '8da6959e-7c9e-4775-870b-15ff17069d2c';
+
+// TODO: Update WEBHOOK_VERIFICATION_ID with your WEBHOOK VERIFICATION ID (can be found under Workspace/OAuth)
+const WEBHOOK_VERIFICATION_ID = '430ba021-fd04-47fe-8f1c-2b8a72331b40';
+
+const AUTH_PROVIDER_BASE_URL = 'https://www.wix.com/oauth';
+const INSTANCE_API_URL = 'https://dev.wix.com/api/v1';
+
+const app = express();
+const port = process.env.PORT || 3000;
+const incomingWebhooks = [];
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'statics')));
+
+function getTokensFromWix (authCode) {
+  return axios.post(`${AUTH_PROVIDER_BASE_URL}/refreshTokens`, {
+    authorizationCode: authCode,
+    appSecret: APP_SECRET,
+    appId: APP_ID,
+  }).then((resp) => resp.data);
+}
+
+function getAccessToken (refreshToken) {
+  return axios.post(`${AUTH_PROVIDER_BASE_URL}/accessTokens`, {
+    refreshToken,
+    appSecret: APP_SECRET,
+    appId: APP_ID,
+  }).then((resp) => resp.data);
+}
+
+
+app.post('/webhook-callback', (req, res) => {
+  console.log('got webhook event from Wix:', {data});
+  console.log("===========================");
+  const data = jwt.verify(req.body, WEBHOOK_VERIFICATION_ID);
+  incomingWebhooks.push({body: data, headers: req.headers});
+  res.send(req.body);
+});
+
+app.get('/signup', (req, res) => {
+  // *** PUT YOUR SIGNUP CODE HERE *** ///
+  console.log("got a call from Wix for signup");
+  console.log("==============================");
+
+  const permissionRequestUrl = 'https://www.wix.com/app-oauth-installation/consent';
+  const appId = APP_ID;
+  const redirectUrl = 'https://' + req.get('host') + `/login`;
+  const token = req.query.token;
+  url = `${permissionRequestUrl}?token=${token}&appId=${appId}&redirectUrl=${redirectUrl}`
+
+  console.log("redirecting to " + url);
+  console.log("=============================");
+  res.redirect(url);
+});
+
+app.get('/login',async (req, res) => {
+  // *** PUT YOUR LOGIN CODE HERE *** ///
+  console.log("got a call from Wix for login");
+  console.log("=============================");
+
+  const authorizationCode = req.query.code;
+
+  console.log("authorizationCode = " + authorizationCode);
+
+  let refreshToken, accessToken;
+  try {
+    console.log("getting Tokens From Wix ");
+    console.log("=======================");
+    const data = await getTokensFromWix(authorizationCode);
+
+    refreshToken = data.refreshToken;
+    accessToken = data.accessToken;
+
+    console.log("refreshToken = " + refreshToken);
+    console.log("accessToken = " + accessToken);
+    console.log("=============================");
+
+    instance = await getAppInstance(refreshToken);
+
+    console.log("api call to instance returned: ");
+    console.log(instance);
+
+    // TODO: save instaceId and the tokens for future API calls
+    console.log("=============================");
+    console.log("User's site instanceId: = " + instance.instanceId);
+    console.log("=============================");
+    res.status(200).send("Application " + APP_ID + " was successfully installed on user's site with instanceId = " + instance.instanceId);
+
+  } catch (wixError) {
+    console.log("Error getting token from Wix");
+    console.log({wixError});
+    res.status(500);
+    return;
+  }});
+
+  async function getAppInstance(refreshToken)
+  {
+    try {
+      console.log('getAppInstance with refreshToken = '+refreshToken);
+      console.log("=============================");
+      const {accessToken} = await getAccessToken(refreshToken);
+      console.log('accessToken = ' + accessToken);
+
+      const body = {
+        // *** PUT YOUR PARAMS HERE ***
+        //query: {limit: 10},
+      };
+      const options = {
+        headers: {
+          authorization: accessToken,
+        },
+      };
+      const appInstance = axios.create({
+        baseURL: INSTANCE_API_URL,
+        headers: {authorization: accessToken}
+      });
+      const {instance} = (await appInstance.get('instance', body)).data;
+
+      return instance;
+    } catch (e) {
+      console.log('error in getAppInstance');
+      console.log({e});
+      return;
+    }
+  };
+
+app.get('/', (_, res) => res.status(200).send('Hello Wix!'));
+
+app.listen(port, () => console.log(`My Wix Application is listening on port ${port}!`));
