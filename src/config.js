5f30c52c57aa48ffe53595119f06395135d01d8c
Shay Cohen <shayco@wix.com>
2019-03-11 12:00:32 +0200

    make it better

---
+++ src/config.js
@@ -0,0 +1,12 @@
+// TODO: Update APP_ID with your application ID (can be found in in Wix Developers under Workspace/OAuth)
+const APP_ID = '5fda4ce4-9083-4c2f-a538-40b2bb7fb26a';
+
+// TODO: Update APP_SECRET with your application secret key (can be found in Wix Developers under Workspace/OAuth)
+const APP_SECRET = '3ec89606-0972-4a53-bc47-6b1cf445495a';
+
+// TODO: Update WEBHOOK_VERIFICATION_ID with your WEBHOOK VERIFICATION ID (can be found under Workspace/Webhooks/Public-Key)
+const WEBHOOK_VERIFICATION_ID = '430ba021-fd04-47fe-8f1c-2b8a72331b40';
+
+exports.APP_ID = APP_ID;
+exports.APP_SECRET = APP_SECRET;
+exports.WEBHOOK_VERIFICATION_ID = WEBHOOK_VERIFICATION_ID;
