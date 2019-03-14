981969f7bf3c3da7ae692a7e9c0bdbb54a7b16f9
Shay Cohen <42321113+shaykewix@users.noreply.github.com>
2019-03-14 11:36:34 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // TODO: Update APP_ID with your application ID (can be found in in Wix Developers under Workspace/OAuth)
-const APP_ID = '5fda4ce4-9083-4c2f-a538-40b2bb7fb26a';
+const APP_ID = 'Enter your APP ID here';
 
 // TODO: Update WEBHOOK_VERIFICATION_ID with your WEBHOOK VERIFICATION ID (can be found under Workspace/Webhooks/Public-Key)
-const WEBHOOK_VERIFICATION_ID = '430ba021-fd04-47fe-8f1c-2b8a72331b40';
+const WEBHOOK_VERIFICATION_ID = 'Enter your Webhook ID here';
 
 exports.APP_ID = APP_ID;
 exports.WEBHOOK_VERIFICATION_ID = WEBHOOK_VERIFICATION_ID;
