da905db235f46cd91b951124d9c36f3377c8546b
Shay Cohen <shayco@wix.com>
2019-03-24 13:09:23 +0200

    remove values

--- src/config.js
+++ src/config.js
@@ -1,18 +1,8 @@
 // TODO: Update APP_ID with your application ID (can be found in in Wix Developers under Workspace/OAuth)
-//const APP_ID = 'Enter your APP ID here';
-const APP_ID = 'a9e09516-230f-4855-9449-698f07061193';
+const APP_ID = 'Enter your APP ID here';
 
 // TODO: Update PUBLIC_KEY with your public Key (can be found under Workspace/Webhooks/Public-Key)
-//const PUBLIC_KEY = 'Enter your PUBLIC KEY here';
-const PUBLIC_KEY = `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApKkbbqlSAz/lkrlej7ZM
-goJCgXHkIk97qkmBq94U8Vp9CiuWIpWe4uIothqH786NqSoePAj5TXZcx+lc2G5p
-kvMpSNzJamSkFSlIWHOyT+Izz/nofrka5Hj84eu4rjQxEAHL/gRzY2yVET+IoK5i
-3MDY2o2cS+E7nnuPU9A9uMpdZWsyBnTAuQD8SmIF1DC57XpwOGoXiolJYhC4ckvr
-EaM35IZwrklzm/9L9mgIqDYsJwqAk/g00u3Vw28KbZA5G2qy2+Bwdi2yJ1KfkjBI
-9z7CHLu47xV2EM7JJcGcwZdXUrhyTuqz+ZvdLlLdTn8HpSKxjAFCheEI0yBm1K8+
-RwIDAQAB
------END PUBLIC KEY-----`;
+const PUBLIC_KEY = 'Enter your PUBLIC KEY here';
 
 exports.APP_ID = APP_ID;
 exports.PUBLIC_KEY = PUBLIC_KEY;
