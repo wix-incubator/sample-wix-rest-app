2e623c56efca356d7b5960d0cefb137e1082df44
Shay Cohen <shayco@wix.com>
2019-05-01 17:46:36 +0300

    fix doc links

--- src/credentials.js
+++ src/credentials.js
@@ -1,5 +1,4 @@
 // TODO: Update APP_SECRET with your application secret key (can be found in Wix Developers under Workspace/OAuth)
-//const APP_SECRET = 'App Secret Goes Here';
-const APP_SECRET = 'b9d87029-22b1-48d8-aa6e-c22947be0323';
+const APP_SECRET = 'App Secret Goes Here';
 
 exports.APP_SECRET = APP_SECRET;
